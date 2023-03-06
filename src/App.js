c13ee979e2e6be5f4ae0771588605470522a5891
Lais Betim <laisbetim16@gmail.com>
2023-03-06 14:49:16 -0300

    inicio

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="cabecalho">
+        <h1>Rick and Morty API</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
